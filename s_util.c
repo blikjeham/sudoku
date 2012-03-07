fdc61407d395c2c06dd5e8211b5c53a7c5b653f1
Wybe van der Ham <wybe@snth.eu>
2012-03-07 21:24:44 +0100

    Simplified mtov and vtom functions.

--- s_util.c
+++ s_util.c
@@ -547,72 +547,17 @@ int mtop(int mask)
 
 int vtom(int value)
 {
-	switch(value) {
-	case 1:
-		return(ONE);
-		break;
-	case 2:
-		return(TWO);
-		break;
-	case 3:
-		return(THREE);
-		break;
-	case 4:
-		return(FOUR);
-		break;
-	case 5:
-		return(FIVE);
-		break;
-	case 6:
-		return(SIX);
-		break;
-	case 7:
-		return(SEVEN);
-		break;
-	case 8:
-		return(EIGHT);
-		break;
-	case 9:
-		return(NINE);
-		break;
-	default:
-		return(-1);
-	}
+	return(1 << (value-1));
 }
 
 int mtov(int mask)
 {
-	switch(mask) {
-	case ONE:
-		return(1);
-		break;
-	case TWO:
-		return(2);
-		break;
-	case THREE:
-		return(3);
-		break;
-	case FOUR:
-		return(4);
-		break;
-	case FIVE:
-		return(5);
-		break;
-	case SIX:
-		return(6);
-		break;
-	case SEVEN:
-		return(7);
-		break;
-	case EIGHT:
-		return(8);
-		break;
-	case NINE:
-		return(9);
-		break;
-	default:
-		return(-1);
+	int i;
+	for (i = 0; i < 10; i++) {
+		if ((mask >> i) == 0)
+			return(i);
 	}
+	return(-1);
 }
 
 
